84031013dfbd1ad293f6cae34bef69a0d1cfc2da
Esteban Rodriguez <estebanrodriguez@sandiego.edu>
2024-12-09 17:55:38 -0800

    got rid of unused code

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify, render_template
-from pb_api import get_embed_token
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,24 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/embed-info", methods=["GET"])
-def get_embed_info():
-    try:
-        embed_token = get_embed_token()
-        return jsonify(embed_token)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-"""@app.route("/data", methods=["GET"])
-def get_data():
-    query = "SELECT * FROM your_table"
-    with engine.connect() as connection:
-        result = connection.execute(query)
-        data = [dict(row) for row in result]
-    return jsonify(data)"""
 
 
 if __name__ == "__main__":
